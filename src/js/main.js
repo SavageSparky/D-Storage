7d0dc36d17e9f1b482ac316244ecc4a98758a67a
lakshmikant <lakshmikant2512@gmail.com>
2021-12-29 10:31:34 +0530

    add firebase setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 //import css 
 import "../css/style.css"
 import "../css/utils.css"
+
+//import js
+import "./modules/firebase-key"
---
+++ src/js/modules/firebase-key.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkuxKxbnCb_VMd-7ZtEVdVExpZXt-YDBY",
+    authDomain: "d-storage-38e8c.firebaseapp.com",
+    projectId: "d-storage-38e8c",
+    storageBucket: "d-storage-38e8c.appspot.com",
+    messagingSenderId: "744876836024",
+    appId: "1:744876836024:web:87a8d49d76df7ea48f604d"
+  };
+
+const app = initializeApp(firebaseConfig);
