522343a78b3873531677fe4773ca1dc8a0dd36f6
Akilan <akilan73210@gmail.com>
2022-01-01 11:12:17 +0530

    added files section

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,16 @@ const status = document.querySelector('.description');
 const uploadButton = document.getElementById('submit');
 uploadButton.addEventListener('click', async (event)=> {
   event.preventDefault();
-  
+
   const files = document.getElementById('file_selector').files;
   console.log(files);
 
-  status.s
+  status.style.color = "#0351c5";
   status.innerHTML = "Please Wait, Uploading Files..."
 
   let cid = await storeFiles(files);
   console.log("from index.js : "+cid);
 
+  status.style.color = "green";
   status.innerHTML = `File uploaded! <br> cid generated! <br> Go to Files Section for more info`;
 })
