22c0a7023910b2a2b98688e5d73b16ec2c105359
akilan <akilan73210@gmail.com>
2022-02-25 12:45:07 +0530

    minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import "../css/style.css"
 import "../css/utils.css"
 
 //import js
+import "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"
 import "./modules/firebase-key"
 import "./firebase-authentication"
 import "./user-det"
